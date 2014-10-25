6bf1753f5f94f5222b39c5b517f16c03f8f2e10a
Nantas Nardelli <nantas.nardelli@gmail.com>
2014-10-25 17:33:48 +0100

    Book version of parsing.c

--- parsing.c
+++ parsing.c
@@ -29,40 +29,22 @@ void add_history(char* unused) {}
 
 int main(int argc, char** argv) {
 
-  /* mpc_parser_t* Number = mpc_new("number"); */
-  /* mpc_parser_t* NumOperator = mpc_new("num_operator"); */
-  /* mpc_parser_t* LitOperator = mpc_new("lit_operator"); */
-  /* mpc_parser_t* Operator = mpc_new("operator"); */
-  /* mpc_parser_t* Expr = mpc_new("expr"); */
-  /* mpc_parser_t* Nthlisp = mpc_new("nthlisp"); */
-  mpc_parser_t* Adjective = mpc_new("adjective");
-  mpc_parser_t* Noun = mpc_new("noun");
+  mpc_parser_t* Number = mpc_new("number");
+  mpc_parser_t* Operator = mpc_new("operator");
   mpc_parser_t* Expr = mpc_new("expr");
   mpc_parser_t* Nthlisp = mpc_new("nthlisp");
+
   mpca_lang(MPCA_LANG_DEFAULT,
-            /* "                                                           \ */
-            /*   number   : /-?[0-9]+/'.'?/[0-9]+/ ;                       \ */
-            /*   num_operator : '+' | '-' | '*' | '/' | '%' ;              \ */
-            /*   lit_operator : /add/ | /sub/ | /div/ | /mul/ ;            \ */
-            /*   operator: <num_operator> | <lit_operator> ;               \ */
-            /*   expr     : <number> | '(' <operator> <expr>+ ')' ;        \ */
-            /*   nthlisp  : /^/ <operator> <expr>+ /$/ ;                   \ */
-            /* ", */
-            /* "                                                           \ */
-            /*   number   : /-?[0-9]+/;                                    \ */
-            /*   num_operator : '+' | '-' | '*' | '/' | '%' ;              \ */
-            /*   lit_operator : /add/ | /sub/ | /div/ | /mul/ ;            \ */
-            /*   operator: <num_operator> | <lit_operator> ;               \ */
-            /*   expr     : <number> | '(' <number> <operator> <expr> ')' ; \ */
-            /*   nthlisp  : /^/ <number> <operator> <expr> /$/ ;         \ */
-            /* ", */
-            "                                                       \
-              noun : /lisp/ | /language/ | /c/ | /book/ | /build/ ; \
-              adjective : /wow/ | /many/ | /so/ | /such/ ;          \
-              expr : <adjective> <noun> ;                           \
-              nthlisp : <expr>+ ;                                   \
+            "                                                           \
+              number   : /-?[0-9]+/ ;                                   \
+              operator : '+' | '-' | '*' | '/' | '%' ;                  \
+              expr     : <number> | '(' <operator> <expr>+ ')' ;        \
+              nthlisp  : /^/ <operator> <expr>+ /$/ ;                   \
             ",
-            Noun, Adjective, Expr, Nthlisp);
+            Number,
+            Operator,
+            Expr,
+            Nthlisp);
 
   puts("Nthlisp Version 0.0.0.3");
   puts("Press Ctrl+c to exit\n");
@@ -70,12 +52,12 @@ int main(int argc, char** argv) {
   while (1) {
     char* input = readline("nthlisp> ");
     add_history(input);
-    /* printf("%s\n", input); */
 
     mpc_result_t r;
     if (mpc_parse("<stdin>", input, Nthlisp, &r)) {
      /* On Success Print the AST */
       mpc_ast_print(r.output);
+      /* Load AST from output */
       mpc_ast_delete(r.output);
     } else {
       /* Otherwise Print the Error */
@@ -84,7 +66,10 @@ int main(int argc, char** argv) {
     }
     free(input);
   }
-  mpc_cleanup(6, Adjective, Noun, Expr, Nthlisp);
-            //Number, Operator, Expr, NumOperator, LitOperator, Nthlisp);
+  mpc_cleanup(6,
+              Number,
+              Operator,
+              Expr,
+              Nthlisp);
   return 0;
 }
